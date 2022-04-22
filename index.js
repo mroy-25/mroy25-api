c0de86f63a712f5d9554e026b11bb283916d6879
mroy-25 <78289020+mroy-25@users.noreply.github.com>
2022-04-22 04:00:43 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const { nhdetail, nhartist, nhtag, nhhome, nhsearch, nhDl } = require('./lib/nhentai')
+
+
+module.exports.nhdetail = nhdetail
+module.exports.nhartist = nhartist
+module.exports.nhtag = nhtag
+module.exports.nhhome = nhhome
+module.exports.nhsearch = nhsearch
+module.exports.nhDl = nhDl
+
+
+
